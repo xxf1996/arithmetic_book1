b9467a60b90378e31e4bb0f5adfa5d27a10671a2
xuefeng <1158231926@qq.com>
2018-05-08 19:26:38 +0800

    fixed: update day.js and change some content of Q07

--- Q07/Q07.js
+++ Q07/Q07.js
@@ -9,20 +9,15 @@ function is_meet(t) {
 function for_question() {
     let begin = dayjs('1964-10-10');
     let end = dayjs('2020-07-25');
-    let sp = dayjs('1970-1-1');
     let result = [];
 
     while(begin.isBefore(end)){
         if(is_meet(begin.format('YYYYMMDD'))) result.push(begin.format('YYYYMMDD'));
-        if(begin.isSame(sp)){ // 貌似1970-1-1往后增加一天会有bug，需要手动设置
-            begin = dayjs('1970-1-2');
-        }else{
-            begin = begin.add(1, 'day');
-        }
+        begin = begin.add(1, 'day');
     }
 
     console.log(result);
 }
 
 for_question();
-console.log(dayjs('1970-1-1').add(1, 'day').format('YYYYMMDD'));
+console.log(dayjs('1970-1-1').add(1, 'day').format('YYYYMMDD')); // bug已修复
